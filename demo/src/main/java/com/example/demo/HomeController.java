54491b02c0d85f8abd7d15ac278f2acd94116c8e
awalfayez <59050993+awalfayez@users.noreply.github.com>
2019-12-19 13:51:39 +0300

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String greeting() {
+        return "Welcome to the secured web page";
+    }
+
+}
